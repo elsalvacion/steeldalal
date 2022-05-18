ed0592a677a40fc46590872f36507406e870c9dc
elsalvacion <elsalvacion2022@gmail.com>
2022-05-19 00:50:47 +0600

    ready for test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,20 +3,10 @@ import store from './store'
 import {HashRouter, Route, Switch} from 'react-router-dom'
 import HomeScreen from './screens/HomeScreen';
 import Nav from './components/layout/Nav';
-import { useEffect } from 'react';
 import Footer from './components/layout/Footer';
 import ProductScreen from './screens/ProductScreen';
 const App = () => {
-  useEffect(() => {
-    const body = document.querySelector('.app');
-    const footer = document.querySelector('.footer')
-    body.style.paddingBottom = `${footer.clientHeight + 50}px`
-    window.addEventListener('resize', () => {
-      const body = document.querySelector('.app');
-    const footer = document.querySelector('.footer')
-    body.style.paddingBottom = `${footer.clientHeight + 50}px`
-    })
-  }, [])
+ 
   return (
     <Provider store={store}>
       <HashRouter>
