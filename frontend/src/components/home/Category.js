f5b384b33e86d84176d0869383cc5abe805db347
elsalvacion <elsalvacion2022@gmail.com>
2022-05-22 00:27:40 +0600

    home page edited again

--- frontend/src/components/home/Category.js
+++ frontend/src/components/home/Category.js
@@ -1,44 +1,20 @@
 import React from 'react'
 import { allCategories } from '../../constants/category'
-import AliceCarousel from 'react-alice-carousel';
-import 'react-alice-carousel/lib/alice-carousel.css';
 import './Category.css'
+import {Link} from 'react-router-dom'
+import {Typography} from '@mui/material'
 const Category = () => {
-const currentCategory = 'Latest Products'
-const items =  allCategories.map(category => <button className={`allCategoryItem ${category.title === currentCategory && 'categoryItemActive'}`} key={category.id}>
-{category.title}
-</button>)
+
   return (
     <div className='categoriesMainContainer'>
         <h2>Categories</h2>
-        <AliceCarousel
-     mouseTracking 
-     autoHeight={true}
-     autoPlayInterval={2000}
-     disableButtonsControls={true}
-     items={items}
-     controlsStrategy="responsive"
-     responsive={{
-        0: {
-            items: 3,
-        },
-        350: {
-          items: 4,
-      },
-        600: {
-            items: 5,
-        },
-        900: {
-          items: 6,
-      },
-        1024: {
-            items: 7
-        },
-        1750: {
-          items: 10
-      }
-      }}
-     />
+        <div className="categoryItemsContainer">
+          {
+            allCategories.map(category => <Link key={category.id} to='' className='categoryItem'>
+              <Typography variant='h6' component='h6'>{category.title}</Typography>
+            </Link>)
+          }
+        </div>
     </div>
   )
 }
--- frontend/src/constants/category.js
+++ frontend/src/constants/category.js
@@ -1,148 +1,28 @@
 export const allCategories = [
     {
-        id: 36,
-        title: 'Latest Products'
-    },
-    {
-        id: 35,
-        title: 'All Products'
-    },
-    {
         id: 1,
-        title: 'Angle'
+        title: 'Steel'
     },
     {
         id: 2,
-        title: 'Billet'
+        title: 'Aluminium'
     },
     {
         id: 3,
-        title: 'Binding Wire'
+        title: 'Sheets'
     },
     {
         id: 4,
-        title: 'Bloom'
+        title: 'Pipes'
     },
     {
         id: 5,
-        title: 'C.R.Coil'
+        title: 'Coils'
     },
     {
         id: 6,
-        title: 'C.R.Sheet'
-    },
-    {
-        id: 7,
-        title: 'CC Profile Sheet'
-    },
-    {
-        id: 8,
-        title: 'Channel'
-    },
-    {
-        id: 9,
-        title: 'Chequered Plate'
-    },
-    {
-        id: 10,
-        title: 'Color Coated Coil'
-    },
-    {
-        id: 11,
-        title: 'Color Plain Sheet'
-    },
-    {
-        id: 12,
-        title: 'F-four'
-    },
-    {
-        id: 13,
-        title: 'F-seven'
-    },
-    {
-        id: 14,
-        title: 'Flat'
-    },
-    {
-        id: 15,
-        title: 'G.C.Sheet'
-    },
-    {
-        id: 16,
-        title: 'G.P.Coil'
-    },
-    {
-        id: 17,
-        title: 'G.P.Sheet'
-    },
-    {
-        id: 18,
-        title: 'Galvalum Coil'
-    },
-    {
-        id: 19,
-        title: 'Galvalum Plain Sheet'
-    },
-    {
-        id: 20,
-        title: 'H.R.Sheet'
-    },
-    {
-        id: 21,
-        title: 'HR Coil'
-    },
-    {
-        id: 22,
-        title: 'Ingot'
-    },
-    {
-        id: 23,
-        title: 'Joist/Beam'
-    },
-    {
-        id: 24,
-        title: 'NPB/WPB'
-    },
-    {
-        id: 25,
-        title: 'Plate'
-    },
-    {
-        id: 26,
-        title: 'Rails'
-    },
-    {
-        id: 27,
-        title: 'Round'
-    },
-    {
-        id: 28,
-        title: 'Serrated Flat'
-    },
-    {
-        id: 29,
-        title: 'Slabs'
-    },
-    {
-        id: 30,
-        title: 'Sponge Iron'
-    },
-    {
-        id: 31,
-        title: 'Square'
-    },
-    {
-        id: 32,
-        title: 'Tee Section'
-    },
-    {
-        id: 33,
-        title: 'TMT Bars'
-    },
-    {
-        id: 34,
-        title: 'Wire Rod'
-    },
+        title: 'Bars'
+    }
     
 ]
 
