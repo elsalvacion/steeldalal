e162d282bb51f5345e10ad3e2e90493d3e10df4a
elsalvacion <elsalvacion2022@gmail.com>
2022-06-22 01:40:08 +0530

    showcase area changed

--- frontend/src/components/home/Showcase.js
+++ frontend/src/components/home/Showcase.js
@@ -1,16 +1,27 @@
-import React from 'react'
-import './Showcase.css'
-import {Link} from 'react-router-dom'
-import banner  from '../../assets/banner_img.png'
+import React from "react";
+import "./Showcase.css";
 const Showcase = () => {
-  
   return (
-    <div className='showcaseContainer'>
-      <Link to=''>
-        <img src={banner} alt="Steeldalal" className='showcaseImage' />
-        </Link>
+    <div className="showcaseContainer">
+      <div className="showcaseContent">
+        <h2>Steeldalal</h2>
+        <p>
+          Hey welcome to steeldalal.com we are now on both web and mobile.
+          <br /> Enjoy your Biz dalala.
+        </p>
+        <a href="https://play.google.com/store/apps/details?id=steel.dalal.com">
+          <img src="assets/google-play.png" alt="steeldalal.com mobile app" />{" "}
+          <h4>Google Play</h4>
+        </a>
+      </div>
+      <a
+        href="https://play.google.com/store/apps/details?id=steel.dalal.com"
+        className="showcaseImage"
+      >
+        <img src="assets/mobile_app.png" alt="steeldalal.com mobile app" />
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default Showcase
+export default Showcase;
