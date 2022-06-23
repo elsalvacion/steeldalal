707e74a97909ed023e2ba4b66d9691634546f1f9
elsalvacion <elsalvacion2022@gmail.com>
2022-06-23 20:23:21 +0530

    port changed to 80

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,7 +6,9 @@ try {
     user: process.env.DB_USER,
     database: process.env.DB_NAME,
     password: process.env.DB_PASS,
+    port: process.env.DB_PORT,
     multipleStatements: true,
+    timeout: 60000,
   });
 
   connection.connect(function (err) {
